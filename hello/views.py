c09b24f5b4a9d0d23cf8b050d6c3f09acc8fb9c2
Taylor Robinson <taylor@appbackr.com>
2016-08-11 16:16:39 -0700

    full handling of POST requests

--- hello/views.py
+++ hello/views.py
@@ -41,90 +41,87 @@ selectRandomRace = "SELECT name FROM races ORDER BY RANDOM() LIMIT 1"
 # Create your views here.
 @csrf_exempt
 def index(request):
+
+	## get the inputs
 	if request.method == 'GET':
 		print "Order up!"
 		print request.GET
 		inputs = dict(request.GET)
+	elif request.method == 'POST':
+		print "POST Order up!"
+		print request.POST
+		inputs = dict(request.POST)
 
-		if 'token' in inputs and inputs['token'][0] == os.environ["APPBACKR_SLACK_TOKEN"]:
+	## process
+	if 'text' in inputs and inputs['text'] != []:
 
-			if 'text' in inputs and inputs['text'] != []:
+		## get list of all given command words
+		text = inputs['text'][0].split(" ")
 
-				## get list of all given command words
-				text = inputs['text'][0].split(" ")
-
-				## first should be my main command
-				greeting_or_farewell = text[0].lower()
-				
-				## second is optional, specifies race
-				if len(text) > 1:
-					if text[1].lower() == "night" and text[2].lower() == "elf":
-						desired_race = "night elf"
-					elif text[1].lower() == "blood" and text[2].lower() == "elf":
-						desired_race = "blood elf"
-					else:
-						desired_race = text[1].lower()
-						cur.execute(checkRace, {'race_name':desired_race})
-						race_id = cur.fetchone()
-						if race_id is None:
-							## return an unknown race error
-							cur.execute(selectAllRaces,)
-							races = cur.fetchall()
-							races_list = [str(race[0])for race in races]
-							return JsonResponse({"text":"Sorry friend, afraid I've never seen specimen of the %(desired_race)s species round these parts.\nWorld of Warcraft races available for you to choose from are: %(races)s"%{"desired_race":desired_race, "races":races_list}})
-						else:
-							race_id = race_id[0]
-				else:
-					cur.execute(selectRandomRace,)
-					desired_race = cur.fetchone()[0]
-
-
-				## check first greeting or farewell or unknown
-				if greeting_or_farewell == "greeting":
-					cur.execute(selectGreetings, {'desired_race':desired_race})
-					wow_message = cur.fetchone()
-					if wow_message is not None:
-						wow_message = wow_message[0]
-						requests.post(inputs['response_url'][0], data=json.dumps({"text":"Master @%(username)s says: %(wow_message)s"%{'username':inputs['user_name'][0], 'wow_message':wow_message}, "response_type":"in_channel"}))
-						return HttpResponse(status=201)
-					else:
-						return JsonResponse({"text":"I'm ever so sorry Master @%(username)s.  It appears my tomes have gotten mixed up.  Please query my wisdom later. :crystal_ball:"%{'username':inputs['user_name'][0]}})
-
-				elif greeting_or_farewell == "farewell":
-					cur.execute(selectFarewells, {'desired_race':desired_race})
-					wow_message = cur.fetchone()
-					if wow_message is not None:
-						wow_message = wow_message[0]
-						requests.post(inputs['response_url'][0], data=json.dumps({"text":"Master @%(username)s says: %(wow_message)s"%{'username':inputs['user_name'][0], 'wow_message':wow_message}, "response_type":"in_channel"}))
-						return HttpResponse(status=201)
-					else:
-						return JsonResponse({"text":"I'm ever so sorry Master @%(username)s.  It appears my tomes have gotten mixed up.  Please query my wisdom later. :crystal_ball:"%{'username':inputs['user_name'][0]}})
-
-				elif greeting_or_farewell == "":
-					return JsonResponse({"text":"Try either 'greeting' or 'farewell' and I'll return a random World of Warcraft quote of that type. :crossed_swords:"})
-
-				elif greeting_or_farewell == "races":
+		## first should be my main command
+		greeting_or_farewell = text[0].lower()
+		
+		## second is optional, specifies race
+		if len(text) > 1:
+			if text[1].lower() == "night" and text[2].lower() == "elf":
+				desired_race = "night elf"
+			elif text[1].lower() == "blood" and text[2].lower() == "elf":
+				desired_race = "blood elf"
+			else:
+				desired_race = text[1].lower()
+				cur.execute(checkRace, {'race_name':desired_race})
+				race_id = cur.fetchone()
+				if race_id is None:
+					## return an unknown race error
 					cur.execute(selectAllRaces,)
 					races = cur.fetchall()
-					if races == []:
-						return JsonResponse({"text":"I'm ever so sorry Master @%(username)s.  It appears my tomes have gotten mixed up.  Please query my wisdom later. :crystal_ball:"%{'username':inputs['user_name'][0]}})
-					else:
-						races_list = [str(race[0]) for race in races]
-						return JsonResponse({"text":"World of Warcraft races available for you to choose from are: %(races)s  (Make your selection after 'greeting' or 'farewell')"%{"races":races_list}})
-
+					races_list = [str(race[0])for race in races]
+					return JsonResponse({"text":"Sorry friend, afraid I've never seen specimen of the %(desired_race)s species round these parts.\nWorld of Warcraft races available for you to choose from are: %(races)s"%{"desired_race":desired_race, "races":races_list}})
 				else:
-					return JsonResponse({"text":"Sorry friend, afraid I'm not attuned to \"%(input_text)s\" in these parts.  You'll have better luck with either 'greeting' or 'farewell'. :crossed_swords:."%{'input_text': text[0]}})
+					race_id = race_id[0]
+		else:
+			cur.execute(selectRandomRace,)
+			desired_race = cur.fetchone()[0]
 
-	elif request.method == 'POST':
 
-		print "POST Order up!"
+		## check first greeting or farewell or unknown
+		if greeting_or_farewell == "greeting" or greeting_or_farewell == "greetings":
+			cur.execute(selectGreetings, {'desired_race':desired_race})
+			wow_message = cur.fetchone()
+			if wow_message is not None:
+				wow_message = wow_message[0]
+				requests.post(inputs['response_url'][0], data=json.dumps({"text":"Master @%(username)s says: %(wow_message)s"%{'username':inputs['user_name'][0], 'wow_message':wow_message}, "response_type":"in_channel"}))
+				return HttpResponse(status=201)
+			else:
+				return JsonResponse({"text":"I'm ever so sorry Master @%(username)s.  It appears my tomes have gotten mixed up.  Please query my wisdom later. :crystal_ball:"%{'username':inputs['user_name'][0]}})
 
-		#body_unicode = request.body.decode('utf-8')
-		#body = json.loads(body_unicode)
-		#content = body['content']
-		#print body
+		elif greeting_or_farewell == "farewell" or greeting_or_farewell == "farewells":
+			cur.execute(selectFarewells, {'desired_race':desired_race})
+			wow_message = cur.fetchone()
+			if wow_message is not None:
+				wow_message = wow_message[0]
+				requests.post(inputs['response_url'][0], data=json.dumps({"text":"Master @%(username)s says: %(wow_message)s"%{'username':inputs['user_name'][0], 'wow_message':wow_message}, "response_type":"in_channel"}))
+				return HttpResponse(status=201)
+			else:
+				return JsonResponse({"text":"I'm ever so sorry Master @%(username)s.  It appears my tomes have gotten mixed up.  Please query my wisdom later. :crystal_ball:"%{'username':inputs['user_name'][0]}})
 
-		print request.POST
+		elif greeting_or_farewell == "":
+			return JsonResponse({"text":"Try either 'greeting' or 'farewell' and I'll return a random World of Warcraft quote of that type. :crossed_swords:"})
+
+		elif greeting_or_farewell == "help":
+			return JsonResponse({"text":"Try either 'greeting' or 'farewell' and I'll return a random World of Warcraft quote of that type. :crossed_swords:\nYou can also try 'races' for a list of all the World of Warcraft races you can choose a greeting from."})
+
+		elif greeting_or_farewell == "races":
+			cur.execute(selectAllRaces,)
+			races = cur.fetchall()
+			if races == []:
+				return JsonResponse({"text":"I'm ever so sorry Master @%(username)s.  It appears my tomes have gotten mixed up.  Please query my wisdom later. :crystal_ball:"%{'username':inputs['user_name'][0]}})
+			else:
+				races_list = [str(race[0]) for race in races]
+				return JsonResponse({"text":"World of Warcraft races available for you to choose from are: %(races)s  (Make your selection after 'greeting' or 'farewell')"%{"races":races_list}})
+
+		else:
+			return JsonResponse({"text":"Sorry friend, afraid I'm not attuned to \"%(input_text)s\" in these parts.  You'll have better luck with either 'greeting' or 'farewell'. :crossed_swords:."%{'input_text': text[0]}})
 
 
 
