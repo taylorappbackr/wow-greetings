6b71361c1aabdae2d077d85b0d40bf9dd2b1ebd8
Taylor Robinson <taylor@appbackr.com>
2016-08-11 14:31:39 -0700

    trying to have /auth/success get called above /auth

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,9 +10,9 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^auth', hello.views.auth, name='auth'),
-    url(r'^auth/success', hello.views.auth_success, name='auth_success')
+	url(r'^$', hello.views.index, name='index'),
+	url(r'^db', hello.views.db, name='db'),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^auth/success', hello.views.auth_success, name='auth_success')
+	url(r'^auth', hello.views.auth, name='auth')
 ]
